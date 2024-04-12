0fb29f6a7003ca8e449373f214e85c5da3ffa6a8
Rafael Luke <rafael.lukech@gmail.com>
2024-04-12 20:02:05 -0300

    teste Jest

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Rafael Luke" e o RA "183038"', () => {
+    const nome = "Rafael Luke";
+    const RA = "183038";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
